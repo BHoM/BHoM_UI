9d3a6d850112e734a526c3b911f0bc39a5809151
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-07-05 19:25:57 +0100

    generalise input manipulation and serialisation to Caller

---
+++ UI_Engine/Create/ParamInfo.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Reflection.Attributes;
+using BH.oM.UI;
+using System;
+
+namespace BH.Engine.UI
+{
+    public static partial class Create
+    {
+        /*************************************/
+        /**** Public Methods              ****/
+        /*************************************/
+        [Input("name", "The name of the parameter")]
+        [Input("type", "The framework type of the parameter, e.g. BH.oM.Base.BHoMObject")]
+        [Input("kind", "Whether the parameter is an input of an output. Input is the default value")]
+        [Output("parameter", "The bhom parameter used in the bhom abstract syntax")]
+        public static ParamInfo ParamInfo(string name, Type type = null, ParamKind kind = ParamKind.Input)
+        {
+            if (type == null)
+                type = typeof(object);
+
+            return new ParamInfo
+            {
+                Name = name,
+                DataType = type,
+                Kind = kind
+            };
+        }
+
+        /*************************************/
+    }
+}
