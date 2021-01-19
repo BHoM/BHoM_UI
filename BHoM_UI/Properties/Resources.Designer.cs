d091d6bac95d1aa5521e014b41e07aa439608541
Johanna Olin <Johanna.Olin@BuroHappold.com>
2021-01-19 16:40:08 +0000

    Added new icon to UnitTest

--- BHoM_UI/Properties/Resources.Designer.cs
+++ BHoM_UI/Properties/Resources.Designer.cs
@@ -591,6 +591,18 @@ namespace BH.UI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap UnitTest
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("UnitTest", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
 
