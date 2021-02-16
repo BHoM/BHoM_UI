ab9a09cfec78c22d3682b959e442a543109143d0
adecler <arnaud.declercq@burohappold.com>
2021-02-16 16:22:46 +0800

    Add a SetProjectCode method

---
+++ UI_Engine/Compute/SetProjectCode.cs
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Serialiser;
+using BH.oM.Reflection.Attributes;
+using BH.oM.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace BH.Engine.UI
+{
+    public static partial class Compute
+    {
+        /*************************************/
+        /**** Public Methods              ****/
+        /*************************************/
+
+        [Description(@"Set the code of the project this file is related to.")]
+        [Input("projectCode", "Project code/number this file is associated with.")]
+        [Output("success", "Returns true if the project code was correctly registered.")]
+        public static bool SetProjectCode(string projectCode)
+        {
+            if (projectCode == "")
+                return false;
+
+            Engine.Reflection.Compute.RecordEvent(new ProjectCodeEvent
+            {
+                Message = "The project code for this file is now set to " + projectCode,
+                ProjectCode = projectCode
+            });
+
+            return true;
+        }
+
+        /*************************************/
+    }
+}
+
+
