196f0de060ca221ecf0a910d14761762ca513934
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-08-19 19:12:36 +0100

    Resolve compliance issues on TriggerLogUsageArgs

--- UI_oM/TriggerLogUsageArgs.cs
+++ UI_oM/TriggerLogUsageArgs.cs
@@ -26,14 +26,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BH.oM.Base;
+
 namespace BH.oM.UI
 {
-    public class TriggerLogUsageArgs : EventArgs
+    public class TriggerLogUsageArgs : EventArgs, IObject
     {
-        public virtual object SelectedItem { get; set; }
-        public virtual string CallerName { get; set; }
-        public virtual string UIName { get; set; }
-        public virtual string UIVersion { get; set; }
-        public virtual Guid ComponentID { get; set; }
+        public virtual object SelectedItem { get; set; } = null;
+        public virtual string CallerName { get; set; } = "";
+        public virtual string UIName { get; set; } = "";
+        public virtual string UIVersion { get; set; } = "";
+        public virtual Guid ComponentID { get; set; } = Guid.Empty;
     }
 }
