fc4eac151397a6cb3973533651449f6a28fe5ee5
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-11-01 15:35:56 +0000

    Add ability to query the usage log file name for external toolkits to the UI

---
+++ UI_Engine/Query/UsageLogFolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.UI
+{
+    public static partial class Query
+    {
+        public static string UsageLogFolder()
+        {
+            string logFolder = @"C:\ProgramData\BHoM\Logs";
+
+            //Make sure the folder exists
+            if (!Directory.Exists(logFolder))
+                Directory.CreateDirectory(logFolder);
+
+            return logFolder;
+        }
+    }
+}
