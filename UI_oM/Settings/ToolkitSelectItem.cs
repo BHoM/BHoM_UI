5ceec84304f5735af917ca64ba78e4fb1245163c
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2024-02-23 11:07:08 +0000

    Add settings object for search settings within the UI

---
+++ UI_oM/Settings/ToolkitSelectItem.cs
@@ -0,0 +1,20 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.UI
+{
+    [Description("For each toolkit in BHoM, allocate a boolean flag for whether it should appear in search results or not.")]
+    public class ToolkitSelectItem : BHoMObject
+    {
+        [Description("The name of the toolkit that this item refers to.")]
+        public virtual string Toolkit { get; set; }
+
+        [Description("Determine whether to include the toolkit in search results or not.")]
+        public virtual bool Include { get; set; } = true;
+    }
+}
