4d07764f47d009d73dd2e8f194607f5dae4402c5
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2024-02-23 15:17:59 +0000

    Add Window UI project for containing and handling custom UIs for BHoM implementation

---
+++ BHoM_Windows_UI/Settings/SearchSettingsWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using BH.Engine.Settings;
+using BH.oM.UI;
+using BH.UI.Base.Windows.ViewModel;
+
+namespace BH.UI.Base.Windows.Settings
+{
+
+    public partial class SearchSettingsWindow : Window
+    {
+        public SearchSettingsWindow()
+        {
+            InitializeComponent();
+            LoadSettings();
+            LoadFromLoadedAssemblies();
+
+            m_ToolkitItems = m_ToolkitItems.OrderBy(x => x.Toolkit).ToList();
+            m_ToolkitItems.ForEach(x => ConvertToCheckbox(x));
+
+            this.ShowDialog();
+        }
+
+        private void LoadSettings()
+        {
+            var existingSettings = Query.GetSettings(typeof(BH.oM.UI.SearchSettings)) as SearchSettings;
+            if (existingSettings != null)
+            {
+                m_Settings = existingSettings;
+                m_ToolkitItems.AddRange(existingSettings.Toolkits.Select(x => new ToolkitSelectItemModel(x)));
+            }
+            else
+                m_Settings = new SearchSettings();
+        }
+
+        private void LoadFromLoadedAssemblies()
+        {
+            var allTypes = BH.Engine.Base.Query.AllTypeList();
+            var fullNames = allTypes.Select(x => x.FullName).ToList();
+
+            var toolkits = fullNames.Select(x =>
+            {
+                var split = x.Split('.');
+                if (split.Length < 3)
+                    return null;
+
+                var toolkit = split[2];
+                if (toolkit == "Adapters" || toolkit == "Revit" && split.Length > 3)
+                    toolkit = split[3];
+
+                return toolkit;
+            }).Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+            var toolkitItems = toolkits.Distinct().Select(x =>
+            {
+                return new ToolkitSelectItem()
+                {
+                    Toolkit = x,
+                    Include = true,
+                };
+            }).ToList();
+
+            toolkitItems = toolkitItems.Where(x => !m_ToolkitItems.Any(y => y.Toolkit == x.Toolkit)).ToList();
+            m_ToolkitItems.AddRange(toolkitItems.Select(x => new ToolkitSelectItemModel(x)));
+        }
+
+        private void ConvertToCheckbox(ToolkitSelectItemModel toolkitItem)
+        {
+            CheckBox checkBox = new CheckBox();
+            checkBox.IsChecked = toolkitItem.Include;
+            checkBox.HorizontalAlignment = HorizontalAlignment.Right;
+            checkBox.Margin = new Thickness(10, 0, 0, 0);
+
+            var bind = new Binding();
+            bind.Source = toolkitItem;
+            bind.Path = new PropertyPath("Include");
+            bind.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            bind.Mode = BindingMode.TwoWay;
+            BindingOperations.SetBinding(checkBox, CheckBox.IsCheckedProperty, bind);
+            
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = toolkitItem.Toolkit;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.TextAlignment = TextAlignment.Center;
+            textBlock.Margin = new Thickness(2, 0, 0, 0);
+
+            StackPanel childPanel = new StackPanel();
+            childPanel.Orientation = Orientation.Horizontal;
+            childPanel.Children.Add(checkBox);
+            childPanel.Children.Add(textBlock);
+            childPanel.Margin = new Thickness(0, 0, 0, 10);
+
+            if(m_PanelIndex == 0)
+            {
+                ToolkitCheckboxGridLeft.Children.Add(childPanel);
+                m_PanelIndex++;
+            }
+            else if(m_PanelIndex == 1)
+            {
+                ToolkitCheckboxGridMiddle.Children.Add(childPanel);
+                m_PanelIndex++;
+            }
+            else if(m_PanelIndex == 2)
+            {
+                ToolkitCheckboxGridRight.Children.Add(childPanel);
+                m_PanelIndex = 0;
+            }
+        }
+
+        private void ResetAll(object sender, EventArgs e)
+        {
+            m_ToolkitItems.ForEach(x => x.Include = true);
+        }
+
+        private void SaveAll(object sender, EventArgs e)
+        {
+            m_Settings.Toolkits = m_ToolkitItems.Select(x => x.ToItem()).ToList();
+            BH.Engine.Settings.Compute.SaveSettings(m_Settings, true);
+            this.Close();
+        }
+
+        private int m_PanelIndex = 0;
+
+        private List<ToolkitSelectItemModel> m_ToolkitItems = new List<ToolkitSelectItemModel>();
+        private SearchSettings m_Settings = null;
+    }
+}
---
+++ BHoM_Windows_UI/ViewModel/ToolkitSelectItemModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.UI;
+
+namespace BH.UI.Base.Windows.ViewModel
+{
+    internal class ToolkitSelectItemModel : INotifyPropertyChanged
+    {
+        public ToolkitSelectItemModel(ToolkitSelectItem item)
+        {
+            Toolkit = item.Toolkit;
+            Include = item.Include;
+        }
+
+        public ToolkitSelectItem ToItem()
+        {
+            return new ToolkitSelectItem()
+            {
+                Toolkit = this.Toolkit,
+                Include = this.Include,
+            };
+        }
+
+        private bool m_Include;
+
+        public virtual string Toolkit { get; set; }
+        public virtual bool Include
+        { 
+            get
+            {
+                return m_Include;
+            }
+            set
+            {
+                m_Include = value;
+                NotifyPropertyChanged();
+            }    
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if(PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
