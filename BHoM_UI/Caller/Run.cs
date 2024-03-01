3981f2f2894f81a68e8505abba3ab0a79759527a
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2024-03-01 12:54:14 +0000

    Changes following discussions with @isak-naslundbh and @tom-kingstone

--- BHoM_UI/Caller/Run.cs
+++ BHoM_UI/Caller/Run.cs
@@ -76,15 +76,10 @@ namespace BH.UI.Base
         {
             if (m_CompiledFunc != null)
             {
-                bool suppressingErrors = BH.Engine.Base.Query.IsSuppressingErrors();
-                bool suppressingWarnings = BH.Engine.Base.Query.IsSuppressingWarnings();
-                bool suppressingNotes = BH.Engine.Base.Query.IsSuppressingNotes();
-                bool throwingErrors = BH.Engine.Base.Query.IsThrowingErrors();
-
                 try
                 {
                     var result = m_CompiledFunc(inputs.ToArray());
-                    ResetLogSuppression(suppressingErrors, suppressingWarnings, suppressingNotes, throwingErrors);
+                    BH.Engine.Base.Compute.StopSuppressRecordingEvents();
                     return result;
                 }
                 catch (InvalidCastException e)
@@ -96,12 +91,12 @@ namespace BH.UI.Base
                         MethodInfo method = Engine.Base.Compute.MakeGenericFromInputs(originalMethod, inputs.Select(x => x?.GetType()).ToList());
                         m_CompiledFunc = method.ToFunc();
                         var result = m_CompiledFunc(inputs.ToArray());
-                        ResetLogSuppression(suppressingErrors, suppressingWarnings, suppressingNotes, throwingErrors);
+                        BH.Engine.Base.Compute.StopSuppressRecordingEvents();
                         return result;
                     }
                     else
                     {
-                        ResetLogSuppression(suppressingErrors, suppressingWarnings, suppressingNotes, throwingErrors);
+                        BH.Engine.Base.Compute.StopSuppressRecordingEvents();
                         throw e;
                     }
                 }
@@ -317,16 +312,6 @@ namespace BH.UI.Base
         {
             return true;
         }
-
-        /*************************************/
-
-        protected void ResetLogSuppression(bool suppressingErrors, bool suppressingWarnings, bool suppressingNotes, bool throwingErrors)
-        {
-            BH.Engine.Base.Compute.StopSuppressRecordingEvents(); //Ensure suppression is reset in case the calling method forgot to do it
-            BH.Engine.Base.Compute.StartSuppressRecordingEvents(suppressingErrors, suppressingWarnings, suppressingNotes); //Set the suppression back to what the user may have set it to prior to running this method
-            BH.Engine.Base.Compute.ThrowErrorsAsExceptions(throwingErrors); //Reset throwing errors to whatever the user may have set it to prior to running this method
-        }
-
 
         /*************************************/
         /**** Private Fields              ****/
